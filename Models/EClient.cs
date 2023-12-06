9ec3c5adfad9ac3da32e823ad532b8de18425c60
FilipSzweda <filip.szweda1@gmail.com>
2023-12-06 12:47:12 +0100

    Add Client

---
+++ Models/EClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity_framework.Models
+{
+    internal class EClient: Client
+    {
+        public string IPAddress { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - [Address]: {Address}, [IPAddress]: {IPAddress}";
+        }
+    }
+}
