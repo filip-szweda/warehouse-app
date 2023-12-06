3b1fabcdea5f538369234a5a9c30fc96d5947d94
FilipSzweda <filip.szweda1@gmail.com>
2023-12-06 19:37:45 +0100

    Minor fixes

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -9,8 +9,6 @@ namespace entity_framework.Models
 {
     public class OrderItem
     {
-        [Key]
-        public int Id { get; set; }
         public int OrderId { get; set; }
         public Order Order { get; set; }
 
