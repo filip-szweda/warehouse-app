d0432ebd46633c965075d37cfa57d74b96acf9dd
FilipSzweda <filip.szweda1@gmail.com>
2023-12-06 13:44:30 +0100

    Set active client

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace entity_framework
     {
         private static AppDbContext _dbContext;
         private static ItemWarehouse _itemWarehouse;
+        private static Client _activeClient;
         private static void Main(string[] args)
         {
             _dbContext = new AppDbContext();
@@ -20,6 +21,7 @@ namespace entity_framework
                 Console.WriteLine("1. Add New Item");
                 Console.WriteLine("2. Increase Item Stock");
                 Console.WriteLine("3. Quit");
+                Console.WriteLine("4. Choose Active Client");
 
                 var option = Console.ReadLine();
                 switch (option)
@@ -35,6 +37,9 @@ namespace entity_framework
                         break;
                     case "3":
                         quit = true;
+                        break;
+                    case "4":
+                        _activeClient = ChooseActiveClient();
                         break;
                     default:
                         Console.WriteLine("[ERROR] Invalid option, choose option 0, 1, 2 or 3.");
@@ -82,6 +87,40 @@ namespace entity_framework
             var itemStock = int.Parse(Console.ReadLine());
 
             _itemWarehouse.IncreaseItemStock(itemName, itemStock);
+        }
+
+        private static Client ChooseActiveClient()
+        {
+            string phrase = "";
+            while (true)
+            {
+                var clients = _dbContext.Clients.ToList();
+                if (phrase != "")
+                {
+                    clients = clients.Where(c => c.Name.Contains(phrase)).ToList();
+                }
+                
+                foreach (var client in clients)
+                {
+                    Console.WriteLine(client);
+                }
+                Console.WriteLine("Enter Client index to choose Active Client or enter phrase to filter");
+
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out var index))
+                {
+                    if (index < 0 || index >= clients.Count)
+                    {
+                        Console.WriteLine("[ERROR] Invalid index, choose index between 0 and {0}", clients.Count - 1);
+                        continue;
+                    }
+                    return clients[index];
+                }
+                else
+                {
+                    phrase = input;
+                }
+            }
         }
     }
 }
